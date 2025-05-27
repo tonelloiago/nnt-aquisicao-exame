ae6786eafb3926e696d931c48a670ea9f3cbb8ef
Iago Tonello <tonelloiago@gmail.com>
2025-05-27 23:14:09 +0000

    V2

--- toc.js
+++ toc.js
@@ -8,71 +8,9 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = `
-<ol class="chapter">
-    <li class="chapter-item expanded "><a href="chapters/introducao/introducao.html" target="_parent"><strong aria-hidden="true">1.</strong> Introdução</a></li>
-    <li class="chapter-item expanded "><a href="chapters/acolhimento/acolhimento.html" target="_parent"><strong aria-hidden="true">2.</strong> Acolhimento</a></li>
-    <li class="chapter-item expanded "><a href="chapters/aquisicao/aquisicao.html" target="_parent"><strong aria-hidden="true">3.</strong> Aquisição de exames</a></li>
-    <li>
-        <ol class="section">
-            <li class="chapter-item expanded menu">
-                <a href="chapters/tomografia/tomografia.html" target="_parent"><strong aria-hidden="true">3.1.</strong> Tomografia</a>
-                <ol class="submenu">
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#1-seleção-da-opção-3d" target="_parent"><strongaria-hidden="true"> ▪️</strong> Selecionar opção 3D</a>
-                    </li>
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#2-escolha-do-exame" target="_parent"><strong aria-hidden="true"> ▪️</strong> Escolha do exame</a>
-                                                <li class="chapter-item expanded menu">
-                            <ol class="submenu">
-                                <li>
-                                <li class="chapter-item expanded ">
-                                    <a href="chapters/tomografia/tomografia.html#21-dentição" target="_parent"><strong
-                                            aria-hidden="true">▪️</strong> Dentição</a>
-                                </li>
-                                <li class="chapter-item expanded ">
-                                    <a href="chapters/tomografia/tomografia.html#tomografia-maxilar"
-                                        target="_parent"><strong aria-hidden="true">▪️</strong> Maxilar</a>
-                                </li>
-                                <li class="chapter-item expanded ">
-                                    <a href="chapters/tomografia/tomografia.html#23-articulação-temporomandibular-tmj"
-                                        target="_parent"><strong aria-hidden="true">▪️</strong> ATM</a>
-                                </li>
-                        </li>
-                    </ol>
-                    </li>
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#3-escolha-da-região-anatômica" target="_parent"><strong aria-hidden="true">▪️</strong> Escolha da região anatômica</a>
-                    </li>
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#4-configuração-de-qualidade" target="_parent"><strong aria-hidden="true">▪️</strong> Configuração de qualidade</a>
-                    </li>
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#5-reset-do-aparelho" target="_parent"><strong aria-hidden="true">▪️</strong> Reset do aparelho</a>
-                    </li>
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#6-posicionamento-do-paciente" target="_parent"><strong aria-hidden="true">▪️</strong> Posicionamento do paciente</a>
-                    </li>
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#7-testes-de-scout" target="_parent"><strong aria-hidden="true">▪️</strong> Testes de Scout</a>
-                    </li>
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#8-realização-do-exame" target="_parent"><strong aria-hidden="true">▪️</strong> Execução do exame</a>
-                    </li>
-                    <li class="chapter-item expanded ">
-                        <a href="chapters/tomografia/tomografia.html#9-exportação-da-tomografia" target="_parent"><strong aria-hidden="true">▪️</strong> Exportação da tomografia</a>
-                    </li>
-                </ol>
-                <a href="https://bernardobellezaufrgs.github.io/protocoloOdontoUFRGS/#tomo-1" target="_parent"><strong aria-hidden="true">3.2</strong> Panorâmica</a>
-                <a href="https://bernardobellezaufrgs.github.io/protocoloOdontoUFRGS/#tele-1" target="_parent"><strong aria-hidden="true">3.3</strong> Teleradiografia</a>
-            </li>
-        </ol>
-    </li>
-    <li class="chapter-item expanded "><a href="chapters/especificidades/especificidades.html" target="_parent"><strong aria-hidden="true">4.</strong> Adequações com especificidades da solicitação</a></li>
-</ol>
-`;
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="chapters/introducao/introducao.html"><strong aria-hidden="true">1.</strong> Introdução</a></li><li class="chapter-item expanded "><a href="chapters/acolhimento/acolhimento.html"><strong aria-hidden="true">2.</strong> Acolhimento</a></li><li class="chapter-item expanded "><a href="chapters/aquisicao/aquisicao.html"><strong aria-hidden="true">3.</strong> Aquisição de exames</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="chapters/tomografia/tomografia.html"><strong aria-hidden="true">3.1.</strong> Tomografia</a></li></ol></li><li class="chapter-item expanded "><a href="chapters/especificidades/especificidades.html"><strong aria-hidden="true">4.</strong> Adequações com especificidades da solicitação</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
-        let current_page = document.location.href.toString().split("#")[0];
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
             current_page += "index.html";
         }
@@ -102,7 +40,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
             }
         }
         // Track and set sidebar scroll position
-        this.addEventListener('click', function (e) {
+        this.addEventListener('click', function(e) {
             if (e.target.tagName === 'A') {
                 sessionStorage.setItem('sidebar-scroll', this.scrollTop);
             }
